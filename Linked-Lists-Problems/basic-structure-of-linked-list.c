75b5c84a6baa8b466388b10c32091ff475d883a8
karanshah254 <karankshah2504@gmail.com>
2023-12-14 19:57:33 +0530

    new commit

---
+++ Linked-Lists-Problems/basic-structure-of-linked-list.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void display(struct Node *head)
+{
+    while (head != NULL)
+    {
+        printf("%d\n", head->data);
+        head = head->next;
+    }
+}
+
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+    struct Node *fourth = NULL;
+
+    // memory allocation
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    // assigning values
+    head->data = 10;
+    second->data = 20;
+    third->data = 30;
+    fourth->data = 40;
+
+    // linking node wtih next node
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = NULL;
+
+    // calling display function
+    display(head);
+
+    return 0;
+}
