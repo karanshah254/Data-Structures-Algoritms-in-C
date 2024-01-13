6265e0daa10ffbe6a349863e0017497109f88f8f
karanshah254 <karankshah2504@gmail.com>
2024-01-13 23:53:47 +0530

    new commit

---
+++ public/factorial_using_recurision.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorial(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+
+    return number * factorial(number - 1);
+}
+int main()
+{
+    int number;
+
+    printf("\nEnter Number : ");
+    scanf("%d", &number);
+    if (number < 0)
+    {
+        printf("\nNo factorial is found");
+    }
+    else
+    {
+        printf("\nFactorial of %d is : %d", number, factorial(number));
+    }
+}
