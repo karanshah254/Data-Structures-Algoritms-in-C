9d7597303c2cb7bac9f4b435f4f02b8a138761a9
karanshah254 <karankshah2504@gmail.com>
2024-04-04 16:08:12 +0530

    Added Operating system related problem in C

---
+++ Process-Thread-Scheduling/use_of_fork_call.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+// #include <sys/types.h>
+// #include <unistd.h>
+// #include <sys/wait.h>
+
+// int main()
+// {
+//     printf("Hello World\n");
+//     int y;
+//     int x = fork();
+//     scanf("%d", &y);
+//     printf("It's a beautifull morning\n");
+//     wait(NULL);
+// }
+
+// Run above program in linux system
